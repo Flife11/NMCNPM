0b8fb4aff6c07d8ebc095dd1e4b9116b58199284
21120551 <damtutam@gmail.com>
2023-12-26 13:45:32 +0700

    them mot so file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # NMCNPM
+
+hiện đang code view nên dòng
+app.use("/", async (req, res, next) => {
+    res.render('home');
+});
+trong server.js Sang tự sửa thành cái muốn render, lúc push lên sửa gitignore lại cho tạm ignore server.js đến khi xong views nha
---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require("express");
+const { engine } = require('express-handlebars');
+const http= require('http');
+const app = express();
+
+app.engine('.hbs', engine({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
+
+const port = process.env.PORT | 3000;
+const host = process.env.HOST || 'localhost';
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+// app.use(
+//     session({
+//         resave: true,
+//         saveUninitialized: true,
+//         secret: secret,
+//         cookie: { secure: false },
+//     }),
+// );
+
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).send('Something broke!');
+})
+app.use("/", async (req, res, next) => {
+    res.render('home');
+});
+
+const server = http.createServer(app);   
+server.listen(port, () => console.log(`Server is running at http://${host}:${port}`));
