854e497fe6bf2399099e94c7317875d5702b693e
21120544 <lesangbt99@gmail.com>
2023-12-26 18:39:28 +0700

    rules and period page

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 })
 app.use("/", async (req, res, next) => {
-    res.render('home');
+    res.render('rules');
 });
 
 const server = http.createServer(app);   
